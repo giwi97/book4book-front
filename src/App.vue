230684b8ae4d9415f6acd2d59e5ce102c64a5645
giwi97 <giwanthawijekoon@gmail.com>
2020-09-30 02:33:22 +0530

    Added description

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar app color="white" flat>
+    <v-app-bar color="white" flat>
       <Header />
     </v-app-bar>
-
-    <v-main> </v-main>
+    <v-content>
+      <Hero />
+      <Description />
+    </v-content>
   </v-app>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Description from "./components/Description";
 
 export default {
   name: "App",
 
   components: {
     Header,
+    Hero,
+    Description
   },
 
   data: () => ({
