6998befdc222e4387a81024af82022f8ac07c86c
giwi97 <giwanthawijekoon@gmail.com>
2020-09-30 01:14:40 +0530

    Carousel images

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,23 +1,46 @@
 <template>
-  <v-container>
-    <div class="heroBlock">
-      <v-carousel>
-        <v-carousel-item
-          v-for="(item, i) in items"
-          :key="i"
-          :src="item.src"
-          reverse-transition="fade-transition"
-          transition="fade-transition"
-        ></v-carousel-item>
-      </v-carousel>
-    </div>
-  </v-container>
+  <div class="heroBlock">
+    <v-carousel hide-delimiters cycle>
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      >
+        <v-row class="fill-height title" align="center" justify="center">
+          <div>{{ item.title }}</div>
+        </v-row>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Hero",
 
-  data: () => ({}),
+  data() {
+    return {
+      items: [
+        {
+          src: require("../assets/images/1.jpg"),
+          title: "Getting ready to read again",
+        },
+        {
+          src: require("../assets/images/2.jpg"),
+          title: "The books helping children to get back learning",
+        },
+        {
+          src: require("../assets/images/33.jpg"),
+          title: "Join with us",
+        },
+        {
+          src: require("../assets/images/4.jpg"),
+          title: "Using books to get back to learning",
+        },
+      ],
+    };
+  },
 };
 </script>
