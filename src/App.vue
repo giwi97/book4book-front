e6c26d41a891b1105afd9eda52ff4a5449b0e434
giwi97 <giwanthawijekoon@gmail.com>
2020-09-29 21:10:27 +0530

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-
+    <v-app-bar app color="primary" dark>
+      <Header />
     </v-app-bar>
 
-    <v-main>
-      <Header/>
-    </v-main>
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script>
-import Header from './components/Header';
+import Header from "./components/Header";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     Header,
