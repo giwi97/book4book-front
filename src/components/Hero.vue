b4e89c1ddd23bfe015c651e251e4be326dce92f8
giwi97 <giwanthawijekoon@gmail.com>
2020-09-30 00:09:22 +0530

    Added carousel

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <div class="heroBlock">
+      <v-carousel>
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        ></v-carousel-item>
+      </v-carousel>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+
+  data: () => ({}),
+};
+</script>
