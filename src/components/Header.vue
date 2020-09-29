d3341199d37eca969ec842951e8be38605dd283c
giwi97 <giwanthawijekoon@gmail.com>
2020-09-29 21:26:48 +0530

    toolbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,41 @@
 <template>
   <v-container>
-    Header
+    <v-toolbar
+      dark
+      prominent
+      
+    >
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-toolbar dense>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
   </v-container>
+  
 </template>
 
 <script>
-  export default {
-    name: 'Header',
+export default {
+  name: "Header",
 
-    data: () => ({
-    }),
-  }
+  data: () => ({}),
+};
 </script>
