8cde460dded9b546e607cbe05a91550566b54b54
giwi97 <giwanthawijekoon@gmail.com>
2020-09-29 21:36:37 +0530

    added buttons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,35 +1,49 @@
 <template>
   <v-container>
-    <v-toolbar
-      dark
-      prominent
-      
-    >
-      <v-toolbar-title>Vuetify</v-toolbar-title>
+    <v-toolbar dark prominent>
+      <v-toolbar-title>Book4Book</v-toolbar-title>
 
       <v-spacer></v-spacer>
     </v-toolbar>
-    <v-toolbar dense>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Title</v-toolbar-title>
-
+    <v-toolbar>
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+      <v-toolbar-items>
+        <v-btn text>
+          Home
+        </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+        <v-btn text>
+          Book Request
+        </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+        <v-btn text>
+          Book Offers
+        </v-btn>
+
+        <v-btn text>
+          Events
+        </v-btn>
+
+        <v-btn text>
+          Members
+        </v-btn>
+
+        <v-btn text>
+          Groups
+        </v-btn>
+
+        <v-btn text>
+          How it works
+        </v-btn>
+
+        <v-btn text>
+          About Us
+        </v-btn>
+
+      </v-toolbar-items>
     </v-toolbar>
   </v-container>
-  
 </template>
 
 <script>
