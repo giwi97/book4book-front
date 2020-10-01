0096e9ca102b504c675cde840cf0bb9c4abe58ec
giwi97 <giwanthawijekoon@gmail.com>
2020-10-01 11:26:46 +0530

    Footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,39 @@
 <template>
-  <div class="block latestPostBlock">
-    <v-container>
-      Footer
-    </v-container>
-  </div>
+  <v-container>
+    <v-footer dark padless>
+      <v-card flat tile class="indigo lighten-1 white--text text-center">
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
+          Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
+          accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim
+          a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
+          lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
+          iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum
+          tempor vel ut orci. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>book4book.lk</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-container>
 </template>
 
 <script>
@@ -11,8 +41,7 @@ export default {
   name: "Footer",
 
   data: () => ({
-        items: [
-    ]
+    items: [],
   }),
 };
 </script>
