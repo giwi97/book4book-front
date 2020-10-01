d04e4255e4a77e9092bab6ba886f0bde2b95cc82
giwi97 <giwanthawijekoon@gmail.com>
2020-10-01 13:58:40 +0530

    Header component modified

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,15 @@
   <v-container>
     <v-toolbar prominent flat>
       <v-toolbar-title>Book4Book</v-toolbar-title>
-
       <v-spacer></v-spacer>
+      <div class="text-center" style="padding-top:70px;">
+        <v-btn class="ma-2" rounded color="#26c6da">
+          Sign-up
+        </v-btn>
+        <v-btn class="ma-2" outlined rounded color="#26c6da">
+          Login
+        </v-btn>
+      </div>
     </v-toolbar>
     <v-toolbar flat>
       <v-spacer></v-spacer>
@@ -40,7 +47,6 @@
         <v-btn text>
           About Us
         </v-btn>
-
       </v-toolbar-items>
     </v-toolbar>
   </v-container>
