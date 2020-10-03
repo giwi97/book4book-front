58c0b702c9ac56f7d6ce0bf4078b8d42ae00d9ee
giwi97 <giwanthawijekoon@gmail.com>
2020-10-03 20:24:35 +0530

    Validation script

--- src/components/BookReqForm.vue
+++ src/components/BookReqForm.vue
@@ -68,9 +68,59 @@
 </template>
 
 <script>
+import { required, email, max } from "vee-validate/dist/rules";
+import {
+  extend,
+  ValidationObserver,
+  ValidationProvider,
+  setInteractionMode,
+} from "vee-validate";
+
+setInteractionMode("eager");
+
+extend("required", {
+  ...required,
+  message: "{_field_} can not be empty",
+});
+
+extend("max", {
+  ...max,
+  message: "{_field_} may not be greater than {length} characters",
+});
+
+extend("email", {
+  ...email,
+  message: "Email must be valid",
+});
+
 export default {
   name: "BookReqForm",
 
-  data: () => ({}),
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
+
+  data: () => ({
+    name: "",
+    email: "",
+    select: null,
+    errors: null,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    checkbox: null,
+  }),
+
+  methods: {
+    submit() {
+      this.$refs.observer.validate();
+    },
+    clear() {
+      this.name = "";
+      this.email = "";
+      this.select = null;
+      this.checkbox = null;
+      this.$refs.observer.reset();
+    },
+  },
 };
 </script>
