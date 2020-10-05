e0629d287da3fb7d7348651019d5afa08b4e5c22
giwi97 <giwanthawijekoon@gmail.com>
2020-10-05 23:15:56 +0530

    Minor changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,11 +17,11 @@
         </v-toolbar>
       </div>
       <div class="text-center" style="padding-top:10px;">
-        <v-btn class="ma-2" rounded color="#26c6da">
-          <router-link to="/signup">Sign-up</router-link>
+        <v-btn class="ma-2" rounded color="#26c6da" to="/signup">
+          Sign-up
         </v-btn>
-        <v-btn class="ma-2" outlined rounded color="#26c6da">
-          <router-link to="/login">Login</router-link>
+        <v-btn class="ma-2" outlined rounded color="#26c6da" to="/login">
+          Login
         </v-btn>
       </div>
     </v-toolbar>
