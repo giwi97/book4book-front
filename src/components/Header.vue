e826ec170ea9becfa34bda245e2b1eb91c541f02
giwi97 <giwanthawijekoon@gmail.com>
2020-10-05 22:37:40 +0530

    Changes in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,20 @@
 <template>
   <v-container>
-    <v-toolbar prominent flat color="grey">
+    <v-toolbar flat>
       <v-toolbar-title>Book4Book</v-toolbar-title>
       <v-spacer></v-spacer>
-      <div class="text-center" style="padding-top:70px;">
+      <div class="text-right" style="padding-top:10px;">
+        <v-toolbar flat floating>
+          <v-text-field
+            label="Search"
+            filled
+            rounded
+            dense
+            append-icon="mdi-magnify"
+          ></v-text-field>
+        </v-toolbar>
+      </div>
+      <div class="text-center" style="padding-top:10px;">
         <v-btn class="ma-2" rounded color="#26c6da">
           <router-link to="/signup">Sign-up</router-link>
         </v-btn>
