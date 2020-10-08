5b9ebc17a8cb9a801a6098106eb6e6857df52211
giwi97 <giwanthawijekoon@gmail.com>
2020-10-09 02:36:55 +0530

    Header new

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,44 +1,36 @@
 <template>
   <div style="width:100%;">
-    <v-toolbar
-      flat
-      style="background-image: linear-gradient(to right, black, black);"
-    >
-      <v-img
-        max-height="80"
-        max-width="120"
-        src="../assets/images/logo.png"
-      ></v-img>
-      <v-spacer></v-spacer>
-      <div
-        class="text-right"
-        style="margin-top:25px;background-image: linear-gradient(to right, black, black);"
-      >
-        <v-toolbar
-          flat
-          floating
-          style="background-image: linear-gradient(to right, black, black);"
-        >
-          <v-text-field
-            hide-details
-            single-line
-            label="Search"
-            filled
-            rounded
-            dense
-            append-icon="mdi-magnify"
-          ></v-text-field>
-        </v-toolbar>
-      </div>
-      <div class="text-center" style="margin-top:10px;">
-        <v-btn class="ma-2" rounded color="#FFB300" to="/signup">
-          Sign-up
-        </v-btn>
-        <v-btn class="ma-2" outlined rounded color="#FFB300" to="/login">
-          Login
-        </v-btn>
-      </div>
-    </v-toolbar>
+    <v-card class="mx-auto overflow-visible" style="padding:0px;">
+      <v-app-bar color="white">
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+        <v-toolbar-title>Book4Book</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list nav>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Account</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
   </div>
 </template>
 
@@ -46,6 +38,6 @@
 export default {
   name: "Header",
 
-  data: () => ({}),
+  data: () => ({ drawer: false, group: null }),
 };
 </script>
