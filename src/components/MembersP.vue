dc408bcd6fa31da7a1ddb2ab2e4b6b910ece86d9
giwi97 <giwanthawijekoon@gmail.com>
2020-10-13 02:16:37 +0530

    Members page modifications

--- src/components/MembersP.vue
+++ src/components/MembersP.vue
@@ -1,116 +1,57 @@
 <template>
   <div class="block latestPostBlock">
     <h2 class="text-center">Members</h2>
-    <v-card :loading="isUpdating">
-      <v-row>
-        <v-row class="pa-4" align="center" justify="center">
-          <v-col class="text-center">
-            <h3 class="headline">
-              {{ name }}
-            </h3>
-            <span class="grey--text text--lighten-1">{{ title }}</span>
-          </v-col>
-        </v-row>
-      </v-row>
-      <v-form>
-        <v-container>
-          <v-row>
-            <v-col cols="12">
-              <v-autocomplete
-                v-model="friends"
-                :disabled="isUpdating"
-                :items="people"
-                filled
-                chips
-                color="blue-grey lighten-2"
-                label="Search Members"
-                item-text="name"
-                item-value="name"
-                multiple
-              >
-                <template v-slot:selection="data">
-                  <v-chip
-                    v-bind="data.attrs"
-                    :input-value="data.selected"
-                    close
-                    @click="data.select"
-                    @click:close="remove(data.item)"
-                  >
-                    <v-avatar left>
-                      <v-img :src="data.item.avatar"></v-img>
-                    </v-avatar>
-                    {{ data.item.name }}
-                    {{ data.item.email }}
-                  </v-chip>
-                </template>
-                <template v-slot:item="data">
-                  <template v-if="typeof data.item !== 'object'">
-                    <v-list-item-content
-                      v-text="data.item"
-                    ></v-list-item-content>
-                  </template>
-                  <template v-else>
-                    <v-list-item-avatar>
-                      <img :src="data.item.avatar" />
-                    </v-list-item-avatar>
-                    <v-list-item-content>
-                      <v-list-item-title
-                        v-html="data.item.name"
-                      ></v-list-item-title>
-                      <v-list-item-subtitle
-                        v-html="data.item.group"
-                      ></v-list-item-subtitle>
-                    </v-list-item-content>
-                  </template>
-                </template>
-              </v-autocomplete>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field
-                v-model="friends"
-                :disabled="isUpdating"
-                :items="people"
-                filled
-                color="blue-grey lighten-2"
-                label="Name"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field
-                v-model="title"
-                :disabled="isUpdating"
-                filled
-                color="blue-grey lighten-2"
-                label="Title"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
+    <v-card color="red lighten-2" dark>
+      <v-card-title class="headline red lighten-3">
+        Search for Public APIs
+      </v-card-title>
+      <v-card-text>
+        Explore hundreds of free API's ready for consumption! For more
+        information visit
+        <a
+          class="grey--text text--lighten-3"
+          href="https://github.com/toddmotto/public-apis"
+          target="_blank"
+          >the Github repository</a
+        >.
+      </v-card-text>
+      <v-card-text>
+        <v-autocomplete
+          v-model="model"
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          color="white"
+          hide-no-data
+          hide-selected
+          item-text="Description"
+          item-value="API"
+          label="Public APIs"
+          placeholder="Start typing to Search"
+          prepend-icon="mdi-database-search"
+          return-object
+        ></v-autocomplete>
+      </v-card-text>
       <v-divider></v-divider>
-      <!-- <v-card-actions>
-        <v-switch
-          v-model="autoUpdate"
-          :disabled="isUpdating"
-          class="mt-0"
-          color="green lighten-2"
-          hide-details
-          label="Auto Update"
-        ></v-switch>
+      <v-expand-transition>
+        <v-list v-if="model" class="red lighten-3">
+          <v-list-item v-for="(field, i) in fields" :key="i">
+            <v-list-item-content>
+              <v-list-item-title v-text="field.value"></v-list-item-title>
+              <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-expand-transition>
+      <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn
-          :disabled="autoUpdate"
-          :loading="isUpdating"
-          color="blue-grey darken-3"
-          depressed
-          @click="isUpdating = true"
-        >
-          <v-icon left>
-            mdi-update
+        <v-btn :disabled="!model" color="grey darken-3" @click="model = null">
+          Clear
+          <v-icon right>
+            mdi-close-circle
           </v-icon>
-          Update Now
         </v-btn>
-      </v-card-actions> -->
+      </v-card-actions>
     </v-card>
   </div>
 </template>
