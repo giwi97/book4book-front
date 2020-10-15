7be0f56b0c0f0d8446e7e09f2b6fe1a11a060eab
giwi97 <giwanthawijekoon@gmail.com>
2020-10-15 20:03:35 +0530

    Scroll progress bar

--- src/components/MembersP.vue
+++ src/components/MembersP.vue
@@ -1,7 +1,7 @@
 <template>
-<div>
+  <div>
     <div class="staticHero">
-      <v-img max-height="280" src="../assets/images/img14.jpg">
+      <v-img max-height="230" src="../assets/images/img14.jpg">
         <v-row align="end" class="lightbox white--text pa-2 fill-height">
           <v-col>
             <v-container>
@@ -11,52 +11,56 @@
         </v-row>
       </v-img>
     </div>
-  <div class="block">
-    <h2 class="text-center">Members</h2>
+    <div class="line" id="scrollIndicator"></div>
+    <div class="block">
+      <h2 class="text-center">Members</h2>
 
-    <v-card color="#E0E0E0" width="500" style="margin:auto;">
-
-      <v-card-text>
-        <v-autocomplete
-          v-model="model"
-          :items="items"
-          :loading="isLoading"
-          :search-input.sync="search"
-          color="grey"
-          hide-no-data
-          hide-selected
-          item-text="Name"
-          item-value="API"
-          label="Search Member by Name"
-          placeholder="Start typing to Search"
-          append-icon="mdi-magnify"
-          return-object
-        ></v-autocomplete>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-expand-transition>
-        <v-list v-if="model" class="#E0E0E0 lighten-3">
-          <v-list-item v-for="(field, i) in fields" :key="i">
-            <v-list-item-content>
-              <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
-              <v-list-item-title v-text="field.value"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-expand-transition>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn :disabled="!model" color="red darken-3" @click="model = null">
-          Clear
-          <v-icon right>
-            mdi-close-circle
-          </v-icon>
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-    <br />
-    <br />
-  </div>
+      <v-card color="#37474F" width="500" style="margin:auto;">
+        <v-card-text>
+          <v-autocomplete
+            item-color="grey"
+            filled
+            rounded
+            dark
+            v-model="model"
+            :items="items"
+            :loading="isLoading"
+            :search-input.sync="search"
+            color="primary"
+            hide-no-data
+            hide-selected
+            item-text="Name"
+            item-value="API"
+            label="Search Member by Name"
+            placeholder="Start typing to Search"
+            append-icon="mdi-magnify"
+            return-object
+          ></v-autocomplete>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-expand-transition>
+          <v-list v-if="model" color="#424242" dark>
+            <v-list-item v-for="(field, i) in fields" :key="i">
+              <v-list-item-content>
+                <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
+                <v-list-item-title v-text="field.value"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-expand-transition>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn :disabled="!model" color="red" @click="model = null" dark>
+            Clear
+            <v-icon right>
+              mdi-close-circle
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <br />
+      <br />
+    </div>
   </div>
 </template>
 
