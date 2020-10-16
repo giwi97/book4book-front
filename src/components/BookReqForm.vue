7891e54a03796803e73c0355e349a1355d1697ce
giwi97 <giwanthawijekoon@gmail.com>
2020-10-16 14:08:26 +0530

    Validation and progress bar

--- src/components/BookReqForm.vue
+++ src/components/BookReqForm.vue
@@ -12,111 +12,109 @@
       </v-img>
     </div>
     <div class="block">
-        <div class="descriptionCard">
-          <v-card class="mx-auto lg" max-width="500">
-            <div id="bookreq">Request a Book</div>
-            <v-spacer></v-spacer>
-            <validation-observer ref="observer" v-slot="">
-              <form style="padding:10px;">
-                <validation-provider
-                  v-slot="{ errors }"
-                  name="Name"
-                  rules="required|max:100"
-                >
-                  <v-text-field
-                    v-model="name"
-                    :counter="100"
-                    :error-messages="errors"
-                    label="Name"
-                    required
-                  ></v-text-field>
-                </validation-provider>
-                <validation-provider
-                  v-slot="{ errors }"
-                  name="email"
-                  rules="required|email"
-                >
-                  <v-text-field
-                    v-model="email"
-                    :error-messages="errors"
-                    label="E-mail"
-                    required
-                  ></v-text-field>
-                </validation-provider>
-                <validation-provider
-                  v-slot="{ errors }"
-                  name="address"
-                  rules="required"
-                >
-                  <v-text-field
-                    v-model="address"
-                    :error-messages="errors"
-                    label="Your Address"
-                    required
-                  ></v-text-field>
-                </validation-provider>
-                <validation-provider
-                  v-slot="{ errors }"
-                  name="book"
-                  rules="required"
-                >
-                  <v-text-field
-                    v-model="book"
-                    :error-messages="errors"
-                    label="Book name"
-                    required
-                  ></v-text-field>
-                </validation-provider>
+      <div class="descriptionCard">
+        <v-card class="mx-auto lg" max-width="500">
+          <div id="bookreq">Request a Book</div>
+          <v-spacer></v-spacer>
+          <validation-observer ref="observer" v-slot="">
+            <form style="padding:10px;">
+              <validation-provider
+                v-slot="{ errors }"
+                name="Name"
+                rules="required|max:100"
+              >
+                <v-text-field
+                  v-model="name"
+                  :counter="100"
+                  :error-messages="errors"
+                  label="Name"
+                  id="name"
+                  required
+                ></v-text-field>
+              </validation-provider>
+              <validation-provider
+                v-slot="{ errors }"
+                name="email"
+                rules="required|email"
+              >
+                <v-text-field
+                  v-model="email"
+                  :error-messages="errors"
+                  label="E-mail"
+                  id="email"
+                  required
+                ></v-text-field>
+              </validation-provider>
+              <validation-provider
+                v-slot="{ errors }"
+                name="address"
+                rules="required"
+              >
+                <v-text-field
+                  v-model="address"
+                  :error-messages="errors"
+                  label="Your Address"
+                  id="address"
+                  required
+                ></v-text-field>
+              </validation-provider>
+              <validation-provider
+                v-slot="{ errors }"
+                name="book"
+                rules="required"
+              >
+                <v-text-field
+                  v-model="book"
+                  :error-messages="errors"
+                  label="Book name"
+                  id="book"
+                  required
+                ></v-text-field>
+              </validation-provider>
 
-                <br />
-                <v-row align="end" justify-md="end">
-                  <!-- @click="submit"                  -->
-                  <v-btn
-                    :disabled="dialog"
-                    :loading="dialog"
-                    rounded
-                    color="success"
-                    @click="dialog = true"
-                  >
-                    submit
-                  </v-btn>
-                  <v-btn rounded color="error" @click="clear">
-                    clear
-                  </v-btn>
-                  <v-dialog
-                    v-model="dialog"
-                    hide-overlay
-                    persistent
-                    width="300"
-                  >
-                    <v-card color="primary" dark>
-                      <v-card-text>
-                        Please wait...
-                        <v-progress-linear
-                          indeterminate
-                          color="white"
-                          class="mb-0"
-                        ></v-progress-linear>
-                      </v-card-text>
-                    </v-card>
-                  </v-dialog>
-                </v-row>
-              </form>
-            </validation-observer>
-            <v-progress-linear
-              color="cyan darken-2"
-              rounded
-              value="100"
-            ></v-progress-linear>
-          </v-card>
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-
+              <br />
+              <v-row align="end" justify-md="end">
+                <!-- @click="submit"                  -->
+                <v-btn
+                  :disabled="dialog"
+                  :loading="dialog"
+                  rounded
+                  color="success"
+                  @click="dialog = true"
+                >
+                  submit
+                </v-btn>
+                <v-btn rounded color="error" @click="clear">
+                  clear
+                </v-btn>
+                <v-dialog v-model="dialog" hide-overlay persistent width="300">
+                  <v-card color="primary" dark>
+                    <v-card-text>
+                      Submitting, Please wait...
+                      <v-progress-linear
+                        indeterminate
+                        color="white"
+                        class="mb-0"
+                      ></v-progress-linear>
+                    </v-card-text>
+                  </v-card>
+                </v-dialog>
+              </v-row>
+            </form>
+          </validation-observer>
+          <v-progress-linear
+            color="cyan darken-2"
+            rounded
+            value="100"
+          ></v-progress-linear>
+        </v-card>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   </div>
 </template>
@@ -177,16 +175,18 @@ export default {
     },
   },
   watch: {
-    dialog(val) {
-      this.$refs.observer.validate();
-      if (!this.errors) {
+    async dialog(val) {
+      const isValid =await this.$refs.observer.validate();
+      if (!isValid) {
+        this.dialog = false;
+
+      } else {
+        //return false;
+        
         if (!val) return;
         setTimeout(() => (this.dialog = false), 4000);
         setTimeout(() => location.reload(), 4000);
-      } else {
-        return false;
       }
-      //this.$router.push("/login");
     },
   },
 };
